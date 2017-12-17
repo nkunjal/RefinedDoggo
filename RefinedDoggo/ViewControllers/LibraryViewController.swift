e4fd1460bf9271d82a056743bd677a11183b148e
Neha Kunjal <nkunjal@berkeley.edu>
2017-12-16 17:32:54 -0800

    made the appropriate tabs and files

---
+++ RefinedDoggo/ViewControllers/LibraryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LibraryViewController.swift
+//  RefinedDoggo
+//
+//  Created by Neha Kunjal on 12/16/17.
+//  Copyright © 2017 Neha Kunjal. All rights reserved.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
